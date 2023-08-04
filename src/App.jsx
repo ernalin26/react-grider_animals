f82e6095f265c3763e9b2e3cd0c5fb3e2db3695c
ernalin26 <ernalin26@gmail.com>
2023-08-05 08:24:37 +1000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import './App.css';
 import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
-  return <div>
-    <button onClick={handleClick}>Add Animal</button>
-    <div>Number of animals: {count}</div>
-  </div>;
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
+  return (
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderedAnimals}</div>
+    </div>
+  );
 }
 
 export default App;
